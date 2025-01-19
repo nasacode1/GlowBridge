ad2f74f14d7b4648ea161bf3fdd225fd1882411e
Maanasa Ganesh <maanasarare@gmail.com>
2025-01-19 12:48:43 +0530

    feat: created login page

--- app/src/main/java/com/example/glowbridge/MainActivity.kt
+++ app/src/main/java/com/example/glowbridge/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,21 +22,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.glowbridge.ui.theme.GlowBridgeTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             GlowBridgeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(title = {Text(text = "Glow Bridge")})
+                    }
+
+
+                ) { innerPadding ->
 
                     LoginScreen()
                 }
@@ -44,9 +52,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LoginScreen() {
     var username by remember { mutableStateOf("") } // State variable
-    var isHintVisible by remember {
-        mutableStateOf(true)
-    }
+    var password by remember { mutableStateOf("") }
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -56,28 +62,23 @@ fun LoginScreen() {
     ){
         TextField(
             value =  username, // Binds the state to the TextField
-            onValueChange = { newText -> username = newText
-            if(username != "Enter your username"){
-                isHintVisible = false
-            }
-            }
+            placeholder = { Text(text = "Enter your username")},
+            onValueChange = { newText -> username = newText   }
         )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = password,
+            placeholder = { Text(text = "Enter password")},
+            onValueChange = {newText -> password = newText},
+            visualTransformation = PasswordVisualTransformation())
     }
 }
 
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GlowBridgeTheme {
-        Greeting("Android")
     }
 }
