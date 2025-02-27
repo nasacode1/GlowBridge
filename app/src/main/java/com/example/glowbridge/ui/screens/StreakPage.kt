aeb9e6923286c55987a1f3c855a48d0ed1c02b95
Maanasa Ganesh <maanasarare@gmail.com>
2025-02-27 16:56:29 +0530

    feat: Streak page alignment and added max streak count

--- app/src/main/java/com/example/glowbridge/ui/screens/StreakPage.kt
+++ app/src/main/java/com/example/glowbridge/ui/screens/StreakPage.kt
@@ -4,16 +4,32 @@ import android.content.Context
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.view.SoundEffectConstants
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -22,22 +38,34 @@ import com.example.glowbridge.R
 
 @Composable
 fun StreakPage(){
-    Column(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        val context = LocalContext.current
-        Image(painter = painterResource(id = R.drawable.mainstar), contentDescription ="Main star",
+    Box(modifier = Modifier.fillMaxSize()){
+        Row(
             modifier = Modifier
-                .size(150.dp)
-                .clickable {
-                    val mediaPlayer = MediaPlayer.create(context, R.raw.sparking)
-                    mediaPlayer.start()  // Play audio
-
-                }
-        )
+                .fillMaxWidth()
+                .padding(top = 120.dp)
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(text = "Max streak count : ")
+        }
+        Column (modifier = Modifier.matchParentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center){
+            val context = LocalContext.current
+            var expanded = remember{ mutableStateOf(false)}
+            Image(painter = painterResource(id = R.drawable.mainstar), contentDescription ="Main star",
+                modifier = Modifier
+                    .height(if (expanded.value) 300.dp else 200.dp)
+                    .clickable
+                    {
+                        if (!expanded.value) {
+                            val mediaPlayer = MediaPlayer.create(context, R.raw.sparking)
+                            mediaPlayer.start()
+                            expanded.value = !expanded.value
+                        }
+                    }
+            )
+        }
     }
 }
+
