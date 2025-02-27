d75e5e204e97aa6f6ab5ac4f273d81d4be047e97
Maanasa Ganesh <maanasarare@gmail.com>
2025-02-27 12:27:55 +0530

    feat: Integrate sparkle sound effect to streak icon

--- app/src/main/java/com/example/glowbridge/ui/screens/StreakPage.kt
+++ app/src/main/java/com/example/glowbridge/ui/screens/StreakPage.kt
@@ -1,6 +1,11 @@
 package com.example.glowbridge.ui.screens
 
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.view.SoundEffectConstants
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -10,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.glowbridge.R
@@ -23,7 +29,15 @@ fun StreakPage(){
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(painter = painterResource(id = R.drawable.mainstar), contentDescription ="Main star", modifier = Modifier.size(150.dp))
-    }
+        val context = LocalContext.current
+        Image(painter = painterResource(id = R.drawable.mainstar), contentDescription ="Main star",
+            modifier = Modifier
+                .size(150.dp)
+                .clickable {
+                    val mediaPlayer = MediaPlayer.create(context, R.raw.sparking)
+                    mediaPlayer.start()  // Play audio
 
+                }
+        )
+    }
 }
